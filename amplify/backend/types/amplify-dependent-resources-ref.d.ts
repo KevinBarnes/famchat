009539c98f011451870b6b3af83439dd6cfdfb29
System Administrator <root@Kevins-MacBook-Pro.local>
2022-01-06 11:21:07 -0500

    Added messages API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "famchat": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
